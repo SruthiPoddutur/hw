e8cd93fc4214e9320640a57300fc233eb0fbfe57
Sruthi Poddutur <sruthi_psr@yahoo.com>
2013-03-11 12:25:39 +0530

    Added StaticPages Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Hw::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
   resources :users, :user_sessions
   match 'login' => 'user_sessions#new', :as => :login
   match 'logout' => 'user_sessions#destroy', :as => :logout
